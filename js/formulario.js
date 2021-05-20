e775e798a773c52b667c4036c799ce8476562013
prvaca <palomarodriguezvaca@gmail.com>
2021-05-20 22:05:48 +0200

    VideoPractica4

---
+++ js/formulario.js
@@ -0,0 +1,6 @@
+function cambia_de_pagina(){
+    //var b = document.getElementById("boton").value;
+    //URL = "../index.html" + encodeURIComponent(b)
+    //alert("Gracias por visitar nuestra pagina, nos vamos a el mirador de la serrania")
+    //location.href="http://www.elmiradordelaserrania.com"
+}
